81d1be6b8e0b1e51d5be19c6fa8925a96ad71d03
Desalegn Kasaye <desalegnkasayemuluyekasaye@gmail.com>
2026-07-27 14:31:56 +0300

    perf: simplify PostProcessing - remove ChromaticAberration, Noise, DepthOfField

--- frontend/src/components/projects3d/PostProcessing.jsx
+++ frontend/src/components/projects3d/PostProcessing.jsx
@@ -1,17 +1,11 @@
 import { forwardRef, useMemo } from 'react'
-import { EffectComposer, Bloom, Vignette, ChromaticAberration, ToneMapping, DepthOfField, Noise } from '@react-three/postprocessing'
+import { EffectComposer, Bloom, Vignette, ToneMapping } from '@react-three/postprocessing'
 import { BlendFunction, ToneMappingMode } from 'postprocessing'
-import * as THREE from 'three'
 
 const PostProcessing = forwardRef(function PostProcessing({ isMobile, quality = 'high' }, ref) {
-  const bloomIntensity = isMobile ? 0.4 : quality === 'ultra' ? 1.2 : 0.8
+  const bloomIntensity = isMobile ? 0.4 : quality === 'ultra' ? 1.0 : 0.7
   const luminanceThreshold = isMobile ? 0.4 : 0.2
   const luminanceSmoothing = isMobile ? 1.5 : 1.0
-
-  const chromaticOffset = useMemo(
-    () => new THREE.Vector2(isMobile ? 0.0003 : 0.0008, isMobile ? 0.0003 : 0.0008),
-    [isMobile]
-  )
 
   return (
     <EffectComposer ref={ref} multisampling={isMobile ? 0 : 2}>
@@ -20,35 +14,13 @@ const PostProcessing = forwardRef(function PostProcessing({ isMobile, quality = 
         luminanceThreshold={luminanceThreshold}
         luminanceSmoothing={luminanceSmoothing}
         mipmapBlur
-        radius={isMobile ? 0.3 : 0.6}
+        radius={isMobile ? 0.3 : 0.5}
       />
-      {!isMobile && quality === 'ultra' && (
-        <DepthOfField
-          focusDistance={0.02}
-          focalLength={0.05}
-          bokehScale={2}
-          height={480}
-        />
-      )}
       <Vignette
         offset={0.3}
-        darkness={isMobile ? 0.3 : 0.6}
+        darkness={isMobile ? 0.3 : 0.5}
         blendFunction={BlendFunction.NORMAL}
       />
-      {!isMobile && (
-        <ChromaticAberration
-          offset={chromaticOffset}
-          blendFunction={BlendFunction.NORMAL}
-          radialModulation={true}
-          modulationOffset={0.4}
-        />
-      )}
-      {!isMobile && (
-        <Noise
-          opacity={0.01}
-          blendFunction={BlendFunction.SOFT_LIGHT}
-        />
-      )}
       <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />
     </EffectComposer>
   )
